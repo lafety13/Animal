ee662fabb64b08ed0957527b802cd4166ba5c060
vadim kozak <koozyy@bigmir.net>
2016-05-19 01:13:40 +0300

    animal

---
+++ Animal.php
@@ -0,0 +1,36 @@
+<?php
+
+class Animal
+{
+	private $_name;
+
+	public function __construct($name)
+	{
+		$this->_name = $name;
+	}
+	
+	public function getName()
+	{
+		return $this->_name;
+	}
+
+}
+
+class Cat extends Animal
+{
+	public function meow()
+	{
+		return "Cat {$this->getName()} is saying meow";
+	}
+
+}
+
+$cat = new Cat('garfield');
+
+echo $cat->getName() === 'garfield' && 
+	 $cat->meow() === 'Cat garfield is saying meow' ? true : 0;
+echo "<br>";
+$a = 1;
+$a++;
+
+echo $a++;
